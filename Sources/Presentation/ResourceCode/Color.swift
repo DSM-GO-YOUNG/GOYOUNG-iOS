351d6db08544ff76cde1b4fbb936e66dd20499a7
rlarldud1234 <rlarldud6482@naver.com>
2023-04-10 12:00:57 +0900

    feat :: ResourceCode Color & Image

---
+++ Sources/Presentation/ResourceCode/Color.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let mainGreen = Color("MainGreen")
+    static let mainPink = Color("MainPink")
+    static let line = Color("Line")
+}
