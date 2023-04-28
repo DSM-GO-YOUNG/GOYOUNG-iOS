de57d920a0d9537ff9465b17e23f190f8b187b0a
rlarldud1234 <rlarldud6482@naver.com>
2023-04-28 10:48:07 +0900

    feat :: Asset 설정

--- Sources/Presentation/ResourceCode/Color.swift
+++ Sources/Presentation/ResourceCode/Color.swift
@@ -4,5 +4,7 @@ extension Color {
     static let background = Color("Background")
     static let mainGreen = Color("MainGreen")
     static let mainPink = Color("MainPink")
+    static let deepPink = Color("DeepPink")
     static let line = Color("Line")
+    static let gray100 = Color("Gray100")
 }
--- Sources/Presentation/ResourceCode/Image.swift
+++ Sources/Presentation/ResourceCode/Image.swift
@@ -2,4 +2,14 @@ import SwiftUI
 
 extension Image {
     static let logo = Image("Logo")
+
+    // Tab Icon
+    static let people = Image("People")
+    static let list = Image("List")
+    static let office = Image("Office")
+
+    // Selected Tab Icon
+    static let selectedPeople = Image("SelectedPeople")
+    static let selectedList = Image("SelectedList")
+    static let selectedOffice = Image("SelectedOffice")
 }
