4c9dfd8137b9995b65250fb6f0eb796e40c78551
rlarldud1234 <rlarldud6482@naver.com>
2023-05-01 14:02:00 +0900

    feat :: ApplicatntListView

---
+++ Sources/Presentation/Scene/ApplicantList/ApplicantListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ApplicantListView: View {
+    var body: some View {
+        Text("지원자 리스트")
+    }
+}
+
+struct ApplicantListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApplicantListView()
+    }
+}
