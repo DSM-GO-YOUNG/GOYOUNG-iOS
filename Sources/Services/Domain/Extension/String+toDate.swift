ce5433cd82126d045eed8f46c5bd9ac08374dc30
rlarldud1234 <rlarldud6482@naver.com>
2023-05-15 12:04:02 +0900

    feat :: Extension String + toDate

---
+++ Sources/Services/Domain/Extension/String+toDate.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum DateFormat: String {
+    /// yyyy-MM-dd'T'HH:mm:ss.SSSS
+    case fullDateWithMilliSecondTime = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
+    /// yyyy-MM-dd'T'HH:mm:ss
+    case fullDateWithTime = "yyyy-MM-dd'T'HH:mm:ss"
+    /// yyyy-MM-dd
+    case fullDate = "yyyy-MM-dd"
+}
+
+extension String {
+    func toDate(format: DateFormat) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format.rawValue
+        return formatter.date(from: self)!
+    }
+}
