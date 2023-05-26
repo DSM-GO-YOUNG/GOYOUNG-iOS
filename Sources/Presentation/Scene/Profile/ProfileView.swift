8489532574da738ee6a7cda4d73095089bc9a322
rlarldud1234 <rlarldud6482@naver.com>
2023-05-26 12:02:02 +0900

    feat :: ProfileView

--- Sources/Presentation/Scene/Profile/ProfileView.swift
+++ Sources/Presentation/Scene/Profile/ProfileView.swift
@@ -1,8 +1,44 @@
 import SwiftUI
 
 struct ProfileView: View {
+
+    @StateObject var viewModel = ProfileViewModel()
+
     var body: some View {
-        Text("프로필")
+        NavigationView {
+            VStack(spacing: 20) {
+                AsyncImage(url: viewModel.company?.image) { image in
+                    image
+                        .resizable()
+                        .frame(width: 194, height: 194)
+                } placeholder: {
+                    Color.gray50
+                        .frame(width: 194, height: 194)
+                }
+                .padding(.top, 20)
+                CompanyInfoView(company: viewModel.company)
+                ProfileButton(
+                    image: .write,
+                    title: "모집 공고 작성",
+                    destination: { WriteJobOfferView() }
+                )
+                ProfileButton(
+                    image: .company,
+                    title: "회사 프로필 작성",
+                    destination: { Text("프로필 작성") }
+                )
+                Spacer()
+                NavigationLink(
+                    isActive: $viewModel.notFoundCompany,
+                    destination: { RegistrationCompanyView() },
+                    label: { EmptyView() }
+                )
+            }
+            .onAppear(perform: viewModel.fetchMyCompany)
+            .padding(.horizontal, 16)
+            .navigationTitle("회사 프로필")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
