91cfc39105fcea1edd6572ad1e9e30c27d289c35
rlarldud1234 <rlarldud6482@naver.com>
2023-05-26 12:00:49 +0900

    feat :: ApplicationList

--- Sources/Presentation/Scene/ApplicantList/ApplicantListView.swift
+++ Sources/Presentation/Scene/ApplicantList/ApplicantListView.swift
@@ -1,8 +1,20 @@
 import SwiftUI
 
 struct ApplicantListView: View {
+
+    @StateObject var viewModel = ApplicationListViewModel()
+
     var body: some View {
-        Text("지원자 리스트")
+        NavigationView {
+            List(0..<10, id: \.self) { _ in
+                ApplicationListCell()
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 16, bottom: 0, trailing: 16))
+            }
+            .listStyle(.inset)
+            .navigationTitle("지원자")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
